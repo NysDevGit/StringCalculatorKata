fef179adbea889aae32ae9a831be1a5c62d2be8e
Younouss <toshiroxx@hotmail.fr>
2022-09-26 12:20:44 +0200

    Case with 1 number in input

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,4 +11,31 @@ public class StringCalculatorTest {
 
         Assertions.assertEquals(0,sum);
     }
+
+    @Test
+    public void should_return_one_when_input_number_is_equal_to_one(){
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int sum = stringCalculator.add("1");
+
+        Assertions.assertEquals(1,sum);
+    }
+
+    @Test
+    public void should_return_two_when_input_number_is_equal_to_two(){
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int sum = stringCalculator.add("2");
+
+        Assertions.assertEquals(2,sum);
+    }
+
+    @Test
+    public void should_return_three_when_input_number_is_equal_to_three(){
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int sum = stringCalculator.add("3");
+
+        Assertions.assertEquals(3,sum);
+    }
 }
