96b859f40795724daa045eeec1760f38f6102440
Younouss <toshiroxx@hotmail.fr>
2022-09-26 15:55:20 +0200

    Step 3: Newline separator

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,12 @@
 public class StringCalculator {
+
+    public static final String SEPARATORS = "[,\n]";
+
     public int add(String input) {
         if (input.isEmpty())
             return 0;
         else {
-            String[] numbers = input.split(",");
+            String[] numbers = input.split(SEPARATORS);
             return getSumOf(numbers);
         }
     }
